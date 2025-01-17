1ba74562025700a40ea56303f2ac30edca310bc3
EvaAriza <eva.ariza.cervera@gmail.com>
2025-01-17 10:03:42 +0100

    Add files via upload

---
+++ CONCURSO/CUENTO/script.js
@@ -0,0 +1,307 @@
+$(window).scroll(function () {
+
+
+    //CAJAS//
+
+    //Si el elemento es visible
+    if ($(".caja1").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja1").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja1").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".caja2").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja2").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja2").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".caja3").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja3").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja3"
+        ).removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".caja4").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja4").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja4").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".caja5").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja5").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja5").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".caja6").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja6").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja6").removeClass("visible");
+    }
+
+
+
+    //CAPITULOS// 
+
+    //Si el elemento es visible
+    if ($(".capitulo1").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo1").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo1").removeClass("visible");
+    }
+
+
+    //Si el elemento es visible
+    if ($(".capitulo2").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo2").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo2").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".capitulo3").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo3").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo3").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".capitulo4").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo4").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo4").removeClass("visible");
+    }
+
+
+    //Si el elemento es visible
+    if ($(".capitulo5").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo5").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo5").removeClass("visible");
+    }
+
+
+    //Si el elemento es visible
+    if ($(".capitulo6").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo6").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo6").removeClass("visible");
+    }
+
+
+
+    //IMAGENES//
+
+    // IMAGEN APARECE
+    if ($(".aparece1").visible(true)) {
+        //Añade la clase “visible”
+        $(".aparece1").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".aparece1").removeClass("visible");
+    }
+
+    // IMAGEN APARECE
+    if ($(".aparece2").visible(true)) {
+        //Añade la clase “visible”
+        $(".aparece2").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".aparece2").removeClass("visible");
+    }
+
+    // IMAGEN APARECE
+    if ($(".aparece3").visible(true)) {
+        //Añade la clase “visible”
+        $(".aparece3").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".aparece3").removeClass("visible");
+    }
+
+    // IMAGEN APARECE
+    if ($(".aparece4").visible(true)) {
+        //Añade la clase “visible”
+        $(".aparece4").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".aparece4").removeClass("visible");
+    }
+    // IMAGEN APARECE
+    if ($(".aparece5").visible(true)) {
+        //Añade la clase “visible”
+        $(".aparece5").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".aparece5").removeClass("visible");
+    }
+
+    // IMAGEN APARECE
+    if ($(".aparece6").visible(true)) {
+        //Añade la clase “visible”
+        $(".aparece6").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".aparece6").removeClass("visible");
+    }
+
+
+
+    /*CUENTO2*/
+
+
+    /*CAJAS2*/
+
+    if ($(".caja7").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja7").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja7").removeClass("visible");
+    }
+
+    if ($(".caja8").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja8").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja8").removeClass("visible");
+    }
+
+    if ($(".caja9").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja9").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja9").removeClass("visible");
+    }
+
+    if ($(".caja10").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja10").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja10").removeClass("visible");
+    }
+
+    if ($(".caja11").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja11").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja11").removeClass("visible");
+    }
+
+    if ($(".caja12").visible(true)) {
+        //Añade la clase “visible”
+        $(".caja12").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".caja12").removeClass("visible");
+    }
+
+
+
+
+    /*CAPITULOS*/
+
+    //Si el elemento es visible
+    if ($(".capitulo7").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo7").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo7").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".capitulo8").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo8").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo8").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".capitulo9").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo9").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo9").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".capitulo10").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo10").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo10").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".capitulo11").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo11").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo11").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".capitulo12").visible(true)) {
+        //Añade la clase “visible”
+        $(".capitulo12").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".capitulo12").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".indicador").visible(true)) {
+        //Añade la clase “visible”
+        $(".indicador").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".indicador").removeClass("visible");
+    }
+
+
+
+
+    
+
+});
