9fb68af5c36c4ef491d345c841952b5b32a4f09a
EvaAriza <eva.ariza.cervera@gmail.com>
2025-01-21 19:32:36 +0100

    Add files via upload

---
+++ LANDING/script.js
@@ -0,0 +1,67 @@
+$(window).scroll(function () {
+
+    //Si el elemento es visible
+    if ($(".sobre2-container").visible(true)) {
+        //Añade la clase “visible”
+        $(".sobre2-container").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".sobre2-container").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".sobre1-container").visible(true)) {
+        //Añade la clase “visible”
+        $(".sobre1-container").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".sobre1-container").removeClass("visible");
+    }
+
+
+    //Si el elemento es visible
+    if ($(".portada1").visible(true)) {
+        //Añade la clase “visible”
+        $(".portada1").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".portada1").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".portada2").visible(true)) {
+        //Añade la clase “visible”
+        $(".portada2").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".portada2").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".portada3").visible(true)) {
+        //Añade la clase “visible”
+        $(".portada3").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".portada3").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".textoprincipal").visible(true)) {
+        //Añade la clase “visible”
+        $(".textoprincipal").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".textoprincipal").removeClass("visible");
+    }
+
+    //Si el elemento es visible
+    if ($(".textoprincipal2").visible(true)) {
+        //Añade la clase “visible”
+        $(".textoprincipal2").addClass("visible");
+    } else {
+        //Si no es visible quita:
+        $(".textoprincipal2").removeClass("visible");
+    }
+
+});
